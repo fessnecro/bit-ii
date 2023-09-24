1c7541cdda3a1b66883f607efb6f5aa41e2f8c77
Evgeny <9094705@mail.ru>
2023-09-24 15:05:29 +0700

    ???

--- bot_teach.py
+++ bot_teach.py
@@ -1,9 +1,13 @@
 import openpyxl
 import yake
+import json
+
+import re
 
 from joblib import dump
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.svm import LinearSVC
 
 
@@ -48,13 +52,47 @@ QUESTION_NOT_EXCLUDE = [
     '223-фз',
     'фз-44',
     'фз-223',
-    "кс",
-    'окпд2'
+    'кс',
+    'окпд2',
+    '44-ФЗ',
+    '94-ФЗ',
+    'АРМ',
+    'ЕИС',
+    'ЕАИСТ',
+    'ИНН',
+    'ИСиР',
+    'КПГЗ',
+    'КПП',
+    'КТС',
+    'НДС',
+    'НМЦК',
+    'ОГРН',
+    'ОКПД',
+    'ОКТМО',
+    'ОПО',
+    'СТЕ',
+    'СУДИР',
+    'УПД',
+    'ЭМ',
+    'МО',
+    'ЕАСУЗ'
 ]
+
+ADDITIONAL_QUESTIONS = {
+    "упд": "УПД — это универсальный передаточный документ. Его особенность — многофункциональность, благодаря которой можно заметно уменьшить объем документооборота."
+}
 
 
 
 extractor = yake.KeywordExtractor(lan='ru', stopwords=QUESTION_EXCLUDE, n=3)
+
+
+# as per recommendation from @freylis, compile once only
+CLEANR = re.compile('<.*?>') 
+
+def cleanhtml(raw_html):
+  cleantext = re.sub(CLEANR, '', raw_html)
+  return cleantext
 
 
 def fillDatasheet(intent, question, answer):
@@ -65,7 +103,7 @@ def fillDatasheet(intent, question, answer):
 
         keywords = extractor.extract_keywords(question)
         parts = question.lower().split(" ")
-        notExcludes = [part for part in parts if part in QUESTION_NOT_EXCLUDE]
+        notExcludes = [part for part in parts if part in [x.lower() for x in QUESTION_NOT_EXCLUDE]]
 
         for keyword in keywords:
             if (not INTENTS.get(intent)):
@@ -100,6 +138,38 @@ for row in range(2, worksheet.max_row):
     answer = worksheet.cell(row=row, column=5).value
     fillDatasheet(intent, question, answer)    
 
+for additional in ADDITIONAL_QUESTIONS:
+    fillDatasheet("доплнения", additional.lower(), ADDITIONAL_QUESTIONS[additional].lower()) 
+
+
+# Opening JSON file
+f = open('data3.json', encoding='utf-8')
+ 
+# returns JSON object as
+# a dictionary
+data = json.load(f)
+
+for row in data:
+    for service in row['servicesList']:
+        for article in service['articleList']:
+            intent = row['categoryName']
+            question = article['nameLarge']
+            answer = cleanhtml(article['previewText'])
+            fillDatasheet(intent, question, answer)    
+
+# Opening JSON file
+f = open('data4.json', encoding='utf-8')
+ 
+# returns JSON object as
+# a dictionary
+data = json.load(f)
+
+for row in data:
+    intent = 'Термины и сокращения'
+    question = row
+    answer = data[row]
+    fillDatasheet(intent, question, answer)  
+
 XAnswer = []
 YAnswer = []
 
@@ -108,7 +178,7 @@ for intent in INTENTS:
         XAnswer.append(data['question'])
         YAnswer.append(data['answer'])
 
-vectorizer = TfidfVectorizer(analyzer='char_wb', ngram_range=(2,3), max_df=0.8)
+vectorizer = GradientBoostingClassifier(analyzer='char_wb', ngram_range=(2,3), max_df=0.8)
 vectorizer.fit(XAnswer)
 dump(vectorizer, 'vector-answers.joblib')
 
